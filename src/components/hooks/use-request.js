564cb9b940fcd4e290e7ea0ee0147b4b77774c29
MichaelSpect <michaelsspect@gmail.com>
2022-02-09 20:19:16 +0100

    first commit

---
+++ src/components/hooks/use-request.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+const useRequest = (requestConfig) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const enterTaskHandler = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {};
+};
+
+export default useRequest;
